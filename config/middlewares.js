a78d7f7b290660105c087d50e20c45943f6fea69
Vanessa Pertsch <vanessa.pertsch@posteo.de>
2022-01-27 15:37:49 +0100

    fix for strapi-provider-upload-cloudinary node version error. following: https://www.higithub.com/strapi/issue/strapi/12122

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,19 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -8,4 +21,5 @@ module.exports = [
   'strapi::body',
   'strapi::favicon',
   'strapi::public',
+
 ];
