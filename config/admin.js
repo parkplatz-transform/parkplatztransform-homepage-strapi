4378748277eda69f8436bace8a8c9deac13c76d0
Vanessa Pertsch <vanessa.pertsch@posteo.de>
2022-06-01 15:11:14 +0200

    strapi migration from 4.05 to 4.1.10

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'c685b6a848b6178986d8a270340ded70'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
 });
